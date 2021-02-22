f394ca368f4fd12004de9394c1d87b15d154e292
Kun Patrik <kunpatrik00@gmail.com>
2021-02-22 18:43:12 +0100

    ch13 drill started

---
+++ source/ch13/compile.sh
@@ -0,0 +1,1 @@
+g++ grid.cpp ../_fltk/GUI.cpp ../_fltk/Window.cpp ../_fltk/Graph.cpp ../_fltk/Simple_window.cpp -o grid `fltk-config --ldflags --use-images` -l fltk
---
+++ source/ch13/grid.cpp
@@ -0,0 +1,15 @@
+/*
+    g++ hello_world.cpp ../_fltk/GUI.cpp ../_fltk/Window.cpp ../_fltk/Graph.cpp ../_fltk/Simple_window.cpp -o hello_world `fltk-config --ldflags --use-images` -l fltk
+*/
+
+#include "../_fltk/Simple_window.h"
+#include "../_fltk/Graph.h"
+
+int main()
+{
+	Point topleft{100, 100};
+	Simple_window window{topleft, 800, 1000, "Grid"};
+
+	window.wait_for_button();
+	return 0;
+}
