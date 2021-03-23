a36bded85181c63ef33bd2a3d588873b549c002d
Kun Patrik <kunpatrik00@gmail.com>
2021-03-23 14:45:40 +0100

    ch15 drill halwaydone

--- source/ch15/drill2.cpp
+++ source/ch15/drill2.cpp
@@ -1,7 +1,21 @@
 #include "../std_lib_facilities.h"
 
+struct Person
+{
+
+	string name;
+	int age;
+	string get_name() {return name;}
+	int get_age() {return age;}
+
+};
+
 int main()
 {
+
+	Person x {"Goofy", 63};
+
+	cout << x.get_name() << " - " << x.get_age() << endl;
 
 	return 0;
 }
