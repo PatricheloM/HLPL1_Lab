54f450be74d17f93ae55c67fbda54c4e5e120ff3
Kun Patrik <kunpatrik00@gmail.com>
2021-03-24 13:09:01 +0100

    ch15 drill done

--- source/ch15/drill2.cpp
+++ source/ch15/drill2.cpp
@@ -2,20 +2,97 @@
 
 struct Person
 {
+	private: 	string first_name;
+				string second_name;
+				int age;
+	
+	public:		Person(string c_first_name, string c_second_name, int c_age)
+	{
+		if (c_age >= 0 && c_age < 150)
+		{
+			age = c_age;
+		}
+		else
+		{
+			error("Gondolom...");
+		
+		}
 
-	string name;
-	int age;
-	string get_name() {return name;}
-	int get_age() {return age;}
+		string special_chars = ";:\"\'[]*&^%$#@!.";
+
+		for (int i = 0; i < c_first_name.size(); ++i)
+		{
+			for (int j = 0; j < special_chars.size(); ++j)
+			{
+				if (c_first_name[i] == special_chars[j])
+				{
+					error("Special character in name!");
+				}
+			}
+		}
+
+		for (int i = 0; i < c_second_name.size(); ++i)
+		{
+			for (int j = 0; j < special_chars.size(); ++j)
+			{
+				if (c_second_name[i] == special_chars[j])
+				{
+					error("Special character in name!");
+				}
+			}
+		}
+
+		first_name = c_first_name;
+		second_name = c_second_name;
+	}
+
+	const string get_name() { return first_name + ' ' + second_name; }
+	const int get_age() { return age; }
 
 };
+
+
+istream &operator>>(istream &i, Person &p)
+{
+	string in_first_name;
+	string in_second_name;
+	int in_age;
+
+	i >> in_first_name >> in_second_name >> in_age;
+	p = Person{in_first_name, in_second_name, in_age};
+
+	return i;
+}
+
+ostream &operator<<(ostream &o, Person &p)
+{
+	o << p.get_name() << " - " << p.get_age() << endl;
+
+	return o;
+}
 
 int main()
 {
 
-	Person x {"Goofy", 63};
+	Person buffer {"", "", 0};
 
-	cout << x.get_name() << " - " << x.get_age() << endl;
+	/*
+	cin >> buffer;
+
+	cout << buffer;
+	*/
+
+	vector<Person> persons;
+
+	while (cin >> buffer)
+	{
+		persons.push_back(buffer);
+	}
+
+	for (int i = 0; i < persons.size(); ++i)
+	{
+		cout << persons[i];
+	}
 
 	return 0;
 }
